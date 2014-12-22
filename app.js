0eac64dd8866f8a2850a2d8e1be715874b37d91a
Jake <jake@jakebladt.com>
2014-12-22 14:56:42 -0500

    use handlebars:

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var express = require('express');
 var app = express();
+
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+

423b0515e9942f2f93fa7d610a4dfa39ae12a976
Jake <jake@jakebladt.com>
2014-12-22 14:32:32 -0500

    remove artifacts

--- scripts/walker.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('walkerApp', ['firebase']);
-app.firebaseRef = new Firebase("https://blinding-torch-3484.firebaseio.com/");
-
-app.getUserHive = function(user) {
-  var usersHive = app.firebaseRef.child('users');
-  return usersHive.child(user.uid);
-};

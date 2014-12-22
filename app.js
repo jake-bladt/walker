33592872fe52a6b476445d6981165f3026a4e6a9
Jake <jake@jakebladt.com>
2014-12-22 14:58:14 -0500

    +error handlers

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});

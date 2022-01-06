8f3382cf16b8b82325e1ae7cd0e57ff8dc8cbf2b
Jake Bladt <jake@jakebladt.com>
2022-01-06 13:55:40 -0500

    Add data up to Jan 6, 2022.

--- public/scripts/stepdata.js
+++ public/scripts/stepdata.js
@@ -1,7 +1,12 @@
 // This is a holding place for steps data. It could actually wind up anywhere.
 
 let stepsData = {
-    dailySteps: [
-
-    ]
+    dailyStepCounts: {
+        "20220101": 390,
+        "20220102": 69,
+        "20220103": 594,
+        "20220104": 741,
+        "20220105": 1264,
+        "20220106": 85
+    }
 }

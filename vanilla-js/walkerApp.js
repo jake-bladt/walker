f982a53feb4c462f6d89509078d963eed21f0e99
Jake Bladt <jake@jakebladt.com>
2022-01-06 17:22:23 -0500

    Import temporary data

---
+++ vanilla-js/walkerApp.js
@@ -0,0 +1,13 @@
+let walkerApp = {};
+
+// This will eventually be loaded from the service
+walkerApp.dailyStepCounts = {
+    "20220101": 390,
+    "20220102": 69,
+    "20220103": 594,
+    "20220104": 741,
+    "20220105": 1264,
+    "20220106": 1008
+};
+
+

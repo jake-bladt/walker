4866d56e12a566d0451449d830dfdbce0c56a0f9
Jake Bladt <jake@jakebladt.com>
2022-03-20 20:36:40 -0400

    simple single-item GET

--- lambda-rest-api/aws-lambda/get_handler/index.py
+++ lambda-rest-api/aws-lambda/get_handler/index.py
@@ -3,3 +3,17 @@ import boto3
 def lambda_handler(event, context):
     client = boto3.resource('dynamodb')
     table = client.Table('walker_steps_data')
+
+    response = table.get_item(
+        Key = {
+            'user_date_key': event['id']
+        }
+    )
+
+    if 'Item' in response:
+        return response['Item']
+    else:
+        return {
+            'StatusCode': '404',
+            'body': 'Not found'
+        }
